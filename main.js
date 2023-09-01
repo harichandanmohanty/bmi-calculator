d5e0bc22e2149cc993b07512120820ba23e4f87c
AD\hmohanty <hmohanty@asite.com>
2023-09-01 19:42:08 +0530

    v1

---
+++ main.js
@@ -0,0 +1,27 @@
+const weight = document.querySelector('#weight');
+const height = document.querySelector('#height');
+const calulateButton = document.querySelector('#calulate-bmi');
+const output = document.querySelector('#output');
+
+weight.addEventListener('change', () => {
+    let isValid = /^-?\d+(\.\d+)?$/.test(weight.value);
+    if (!isValid) {
+        alert('Please enter a proper decimal number');
+    }
+});
+
+height.addEventListener('change', () => {
+    let isValid = /^-?\d+(\.\d+)?$/.test(height.value);
+    if (!isValid) {
+        alert('Please enter a proper decimal number');
+    }
+});
+
+function calculateBMI() {
+    return ((weight.value*10000)/(height.value*height.value)).toFixed(3);
+}
+
+calulateButton.addEventListener('click', () => {
+    output.textContent = calculateBMI();
+});
+
