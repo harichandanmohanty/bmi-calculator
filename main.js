9c26920c5969984b7dbe08adbfe82076b57249f1
AD\hmohanty <hmohanty@asite.com>
2023-09-15 18:18:57 +0530

    updated style

--- main.js
+++ main.js
@@ -4,22 +4,26 @@ const calulateButton = document.querySelector('#calulate-bmi');
 const output = document.querySelector('#output');
 const form =document.querySelector('form');
 
-weight.addEventListener('change', () => {
-    let isValid = /^-?\d+(\.\d+)?$/.test(weight.value);
-    if (!isValid) {
-        alert('Please enter a proper decimal number');
-    }
-});
+// weight.addEventListener('change', () => {
+//     if (isNaN(weight.value) || weight.value < 1 || weight.value == "") {
+//         alert('Please enter a proper decimal number');
+//     }
+// });
 
-height.addEventListener('change', () => {
-    let isValid = /^-?\d+(\.\d+)?$/.test(height.value);
-    if (!isValid) {
-        alert('Please enter a proper decimal number');
-    }
-});
+// height.addEventListener('change', () => {
+//     if (isNaN(height.value) || height.value < 1 || weight.value == "") {
+//         alert('Please enter a proper decimal number');
+//     }
+// });
 
 function calculateBMI() {
-    return ((weight.value*10000)/(height.value*height.value)).toFixed(3);
+    if (isNaN(height.value) || height.value < 1 || weight.value == "") {
+        return 'Please enter a proper number';
+    }else if (isNaN(weight.value) || weight.value < 1 || weight.value == "") {
+        return 'Please enter a proper number';
+    } else {
+        return ((weight.value*10000)/(height.value*height.value)).toFixed(3);
+    }
 }
 
 calulateButton.addEventListener('click', () => {
